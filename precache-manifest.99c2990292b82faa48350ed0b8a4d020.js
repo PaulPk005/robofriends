a6e6e6c3b40b1827f4e3d40afcfce634a67fa68e
Paul PK <paulpk005@gmail.com>
2022-10-13 01:16:34 +0300

    Updates

---
+++ precache-manifest.99c2990292b82faa48350ed0b8a4d020.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "db6f10d803a2239a50a2",
+    "url": "/robofriends/static/css/main.6c9bc65a.chunk.css"
+  },
+  {
+    "revision": "db6f10d803a2239a50a2",
+    "url": "/robofriends/static/js/main.db6f10d8.chunk.js"
+  },
+  {
+    "revision": "feeb642a0f0cd70b290b",
+    "url": "/robofriends/static/css/1.b46b4e35.chunk.css"
+  },
+  {
+    "revision": "feeb642a0f0cd70b290b",
+    "url": "/robofriends/static/js/1.feeb642a.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "645da022a349f342f8b891c656e0b3d3",
+    "url": "/robofriends/index.html"
+  }
+];
